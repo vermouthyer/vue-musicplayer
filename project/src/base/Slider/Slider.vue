62b775cb62913785cf72c1a298cac2ac1b340bc9
vermouthyer <553612668@qq.com>
2020-01-09 16:10:21 +0800

    slider setting 2nd

--- project/src/base/Slider/Slider.vue
+++ project/src/base/Slider/Slider.vue
@@ -3,7 +3,14 @@
     <div class="slider-group" ref="sliderGroup">
       <slot></slot>
     </div>
-    <div class="dots"></div>
+    <div class="dots">
+      <span
+        class="dot" 
+        v-for="(item, index) in dots" 
+        :key="index" 
+        :class="{active: currentPageIndex === index}"
+      ></span>
+    </div>
   </div>
 </template>
 
@@ -11,6 +18,12 @@
 import BScroll from 'better-scroll';
 import {addClass} from 'common/js/dom';
 export default {
+  data () {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    }
+  },
   props: {
     loop: {
       type: Boolean,
@@ -22,19 +35,29 @@ export default {
     },
     interval: {
       type: Number,
-      default: 4000
+      default: 2000
     }
   },
   mounted () {
     setTimeout(() => {
       this._setSliderWidth()
+      this._initDots()
       this._initSlider()
+      if (this.autoPlay) {
+        this._autoPlay()
+      }
     }, 20)
+    window.addEventListener('resize', () => {
+      if (!this.slider) {
+        return
+      }
+      this._setSliderWidth(true)
+      this.slider.refresh()
+    })
   },
   methods: {
-    _setSliderWidth () {
+    _setSliderWidth (isResize) {
       this.children = this.$refs.sliderGroup.children
-
       let width = 0;
       let sliderWidth = this.$refs.slider.clientWidth;
       for(let i = 0; i < this.children.length; i++){
@@ -43,10 +66,13 @@ export default {
         child.style.width = sliderWidth + 'px'
         width += sliderWidth
       }
-      if(this.loop){
+      if(this.loop && !isResize){
         width += 2 * sliderWidth
       }
       this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    _initDots () {
+      this.dots = new Array(this.children.length)
     },
     _initSlider () {
       this.slider = new BScroll(this.$refs.slider, {
@@ -57,9 +83,21 @@ export default {
           loop: this.loop,
           threshold: 0.3,
           speed: 400
-        },
-        click: true
+        }
       })
+      this.slider.on('scrollEnd', () => {
+        let pageIndex = this.slider.getCurrentPage().pageX
+        this.currentPageIndex = pageIndex
+        if (this.autoPlay) {
+          clearTimeout(this.timer)
+          this._autoPlay()
+        }
+      })
+    },
+    _autoPlay () {
+      this.timer = setTimeout(() => {
+        this.slider.next()
+      }, this.interval)
     }
   }
 };
@@ -94,4 +132,15 @@ export default {
       bottom: 12px
       text-align: center
       font-size: 0
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+        &.active
+          width: 20px
+          border-radius: 5px
+          background: $color-text-ll
 </style>
